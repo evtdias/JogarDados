0d62041306b9ec57f4ef18a5361a8dad57cc03b9
Everton Dias <evtdias_@hotmail.com>
2021-05-06 17:13:39 -0400

    created UIElements.

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -7,10 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:UIViewController {
+    
+    //MARK: - IBOutlet
+    @IBOutlet weak var dadoImageView2: UIImageView!
+    @IBOutlet weak var dadoImageView1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dadoImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        
+        // dot notation
+        //Who.What = Value
+        dadoImageView1.alpha = 0.5
         
     }
 
